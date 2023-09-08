2bb297f9f403e681cf800d06a5cdda34b208b8b3
Abhishek-kumar00 <111846107+Abhishek-kumar00@users.noreply.github.com>
2023-09-08 07:18:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,77 @@
-var btn=document.getElementById('btn')
+var btn=document.getElementById('btn');
+var todoApp = document.querySelector('.todo-app');
+var timerContainer = document.getElementById('timer-container');
+var timerDisplay = document.getElementById('timer');
+var startButton = document.getElementById('start-button');
+var stopButton = document.getElementById('stop-button');
+var timerInterval;
+var isTimerRunning = false;
 function leftClick(){
     btn.style.left=0;
+    showTodo();
 
 
 }
 function midClick(){
     btn.style.left='65px';
+    showTimer();
+  
 }
 function rightClick(){
     btn.style.left='140px';
 }
+function showTodo() {
+  todoApp.style.display = 'block';
+  timerContainer.style.display = 'none';
+  clearInterval(timerInterval);
+}
+function showTimer() {
+  todoApp.style.display = 'none';
+  timerContainer.style.display = 'block';
+  startTimer();
+}
+function startTimer() {
+  if (!isTimerRunning) {
+      isTimerRunning = true;
+      startButton.disabled = true;
+      stopButton.disabled = false;
+
+      let seconds = 0;
+      let minutes = 0;
+      let hours = 0;
+
+      timerInterval = setInterval(function () {
+          seconds++;
+          if (seconds === 60) {
+              seconds = 0;
+              minutes++;
+              if (minutes === 60) {
+                  minutes = 0;
+                  hours++;
+              }
+          }
+
+          updateTimerDisplay(hours, minutes, seconds);
+      }, 1000);
+  }
+}
+
+function stopTimer() {
+  isTimerRunning = false;
+  clearInterval(timerInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function updateTimerDisplay(hours = 0, minutes = 0, seconds = 0) {
+  const formattedTime = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+  timerDisplay.textContent = formattedTime;
+}
+
+function pad(value) {
+  return value.toString().padStart(2, '0');
+}
+
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 function addTask() {
@@ -46,3 +108,4 @@ function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+showTodo();

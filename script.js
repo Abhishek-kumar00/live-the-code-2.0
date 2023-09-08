8e8c82136fc4a20e13962bef3bb3714e89dc08dd
Abhishek-kumar00 <abhpinran98@gmail.com>
2023-09-08 01:59:07 -0700

    please work

---
+++ script.js
@@ -0,0 +1,48 @@
+var btn=document.getElementById('btn')
+function leftClick(){
+    btn.style.left=0;
+
+
+}
+function midClick(){
+    btn.style.left='65px';
+}
+function rightClick(){
+    btn.style.left='140px';
+}
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask() {
+  if (inputBox.value === "") {
+    alert("you must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();

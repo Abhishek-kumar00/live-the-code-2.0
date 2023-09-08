56c21fb6225f0bec27c95a54ea3a8531050c3939
Abhishek-kumar00 <111846107+Abhishek-kumar00@users.noreply.github.com>
2023-09-08 11:26:33 +0530

    added index.html file

---
+++ index.js
@@ -0,0 +1,12 @@
+var btn=document.getElementById('btn')
+function leftClick(){
+    btn.style.left=0;
+
+
+}
+function midClick(){
+    btn.style.left='60px';
+}
+function rightClick(){
+    btn.style.left='120px';
+}
